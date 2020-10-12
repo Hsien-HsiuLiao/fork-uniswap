3f5e691a91f908b56758f968e5e9e9349fb07a25
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-10-12 12:44:58 -0700

    update gitignore

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fork-uniswap
+in uniswap when you create a market, uniswap uses wrapped ether which is and ERC20 token version of ether
+
+can check etherscan.io for contract code of wrapped ether
---
+++ periphery/migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const Router = artifacts.require("UniswapV2Router02.sol");
+const WETH = artifacts.require("WETH.sol");
+
+module.exports = async function (deployer, network) {
+    let weth;
+    const FACTORY_ADDRESS = '0x2DFEB3210a442e74ca693423C4e838dcCd3A7B83';  //address from contract address field of UniswapV2Factory contract deployment after running truffle migrate in core folder
+
+    if (network === 'mainnet') {
+        weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'); //address on mainnet, found on etherscan
+    } else {
+        await deployer.deploy(WETH);
+        weth = await WETH.deployed();
+    }
+
+    await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
+};
